7dfd48be17ab0b883281d88bfe078deefd36145b
rwrud <bosesbjorn@gmail.com>
2020-12-20 01:22:03 -0600

    changed paths to accomidate windows

--- murder_mystery/play_game.py
+++ murder_mystery/play_game.py
@@ -1,15 +1,17 @@
-from os import listdir
+import os
+from pathlib import Path
 
 from .game import Game
 from .config import Config
 
 def new_game():
-    game_sets = listdir("murder_mystery/game_sets")
+    path = Path('.\murder_mystery\game_sets')
+    games = os.listdir(path)
     g = 0
-    for game_set in game_sets:
-        print("%d. %s" % (g, game_set[:-5].title()))
+    for game in games:
+        print("%d. %s" % (g, game[:-5].title()))
         g += 1
     choice = int(input("Which set would you like to play with?"))
-    config = Config(game_sets[choice])
+    config = Config(games[choice])
     new_game = Game(config)
     new_game.main()
