b3a6e71ecb02fbc1b489a52006aa8d831ed0c182
Robert Ruder <bosesbjorn@gmail.com>
2021-02-09 15:06:22 -0600

    quick fix from last commit. Added choices to room move_to function

--- murder_mystery/rooms.py
+++ murder_mystery/rooms.py
@@ -12,7 +12,7 @@ class Room():
     def move_to(self):
         self.game.investigator.location = self
         text = "You move to %s." % self.name
-        choices = {}
+        choices = game.base_actions
         return text, choices
 
 library = {
